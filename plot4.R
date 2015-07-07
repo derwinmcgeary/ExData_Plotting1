dd9fe93737703ecc15d122f10de5908b5bfbd910
Derwin McGeary <derwinmcgeary+web@gmail.com>
2015-07-07 16:30:24 +0100

    plot4.R now working, corrected goof in plot3.R and am now uploading both pngs

--- plot4.R
+++ plot4.R
@@ -43,9 +43,17 @@ ourdata$DateTime <- paste(ourdata$Date," ",ourdata$Time)
 ourdata$DateTime <- strptime(ourdata$DateTime, "%d/%m/%Y %H:%M:%S")
 ourcols <- ourdata[c(10,7,8,9)]
 ## Plot the data
-png("plot3.png")
+png("plot4.png")
+par(mfrow = c(2,2))
+#plot 1
+with(ourdata, plot(DateTime,Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)"))
+#plot 2
+with(ourdata, plot(DateTime,Voltage, type="l", xlab="datetime", ylab="Voltage"))
+#plot 3
 plot(ourdata$DateTime,ourdata$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
 lines(ourdata$DateTime,ourdata$Sub_metering_2, col="red")
 lines(ourdata$DateTime,ourdata$Sub_metering_3, col="blue")
 legend("topright", legend=colnames(ourdata)[7:9],lty=1, col=c("black","red","blue"))
+#plot 4
+with(ourdata, plot(DateTime,Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power"))
 dev.off()
